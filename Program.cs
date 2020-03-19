86d7bf333822e78908f18244db45623c3b9ebf75
Mayko <mestevez@powerplan.com>
2020-03-19 15:42:10 -0400

    adding timeout approach with task

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConcurrencyLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test Delay
+            var delayTask = new Delay<string>();
+            Console.WriteLine(delayTask.DelayTask("Hola After 5 seconds", TimeSpan.FromSeconds(5)).Result);
+        }
+    }
+}
---
+++ delay.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrencyLab
+{
+    public class Delay<T>
+    {
+        public async Task<T> DelayTask(T result, TimeSpan delay)
+        {
+            await Task.Delay(delay);
+            return result;
+        }
+        public async Task<string> WebRequestTimeOutTask(HttpClient httpClient, string url)
+        {
+            
+            using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(3)))
+            {
+                
+                var downloadTask = httpClient.GetStringAsync(url);
+                var timeOutTask = Task.Delay(Timeout.InfiniteTimeSpan, cts.Token);
+
+                var completedTask = await Task.WhenAny(downloadTask, timeOutTask);
+
+                if (completedTask == timeOutTask)
+                {
+                    return null;
+                }
+
+                return await downloadTask;
+
+            }
+
+        }
+    }
+}
